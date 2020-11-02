2d89f8682e103e0177407e31d35fe6139ee9e2be
Saumya Pandey <sp160899@gmail.com>
2020-11-02 22:09:46 +0530

    Remove unwanted google font links and add static fonts to preload

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,13 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
-import GoogleFonts from "next-google-fonts";
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <GoogleFonts href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" />
         <Head>
           <link href="/static/favicons/favicon.ico" rel="shortcut icon" />
           <link href="/static/favicons/site.webmanifest" rel="manifest" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com/"
-            crossOrigin=""
-          />
-          <link
-            rel="preconnect"
-            href="https://cdn.usefathom.com"
-            crossOrigin=""
-          />
           <link
             href="/static/favicons/apple-touch-icon.png"
             rel="apple-touch-icon"
@@ -45,6 +33,24 @@ class MyDocument extends Document {
           <link
             rel="preload"
             href="/static/fonts/Freight Text Pro/FreightTextProBook-Regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/Freight Text Pro/FreightTextProLight-Regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/GT Walsheim/GTWalsheimPro-Bold.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/static/fonts/GT Walsheim/GTWalsheimPro-Regular.woff2"
             as="font"
             crossOrigin=""
           />
