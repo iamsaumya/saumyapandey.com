99a3c2e44221deb86d54b84cdcb5f9ee4df0bbd5
Saumya Pandey <sp160899@gmail.com>
2020-12-20 18:19:32 +0530

    Add mobile friendly navbar

--- components/NavLink.js
+++ components/NavLink.js
@@ -1,24 +1,29 @@
-import { Button, useColorMode } from "@chakra-ui/core";
+import { Link, useColorMode } from "@chakra-ui/core";
 import NextLink from "next/link";
 
-const NavLink = ({ href, name }) => {
+const NavLink = (props) => {
   const { colorMode } = useColorMode();
+  const { children, isLast, toggleMenu, href = "/", isBlock, ...rest } = props;
+
   const textColor = {
     light: "#334259",
     dark: "#EDEDED",
   };
+
   return (
     <NextLink href={href} passHref>
-      <Button
-        as="a"
-        variant="ghost"
+      <Link
+        onClick={toggleMenu}
+        mr={{ base: 0, sm: isLast ? 0 : 8 }}
         p={[2, 4]}
         color={colorMode === "light" ? textColor.light : textColor.dark}
-        fontSize="18px"
+        fontSize={["1.5rem", "1.7rem"]}
         lineHeight="22px"
+        {...rest}
+        display={isBlock ? "block" : "inline"}
       >
-        {name}
-      </Button>
+        {children}
+      </Link>
     </NextLink>
   );
 };
