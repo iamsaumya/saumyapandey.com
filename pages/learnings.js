acfbaef6cf157bc120245da7c7ecdd03d3778ea6
Saumya Pandey <sp160899@gmail.com>
2021-04-28 11:54:01 +0530

    Fix year

--- pages/learnings.js
+++ pages/learnings.js
@@ -5,15 +5,15 @@ const Learnings = () => {
   return (
     <Stack as="article" fontSize={["1rem", "1.2rem"]}>
       <Box>
-        <Heading mb="4">March 2020</Heading>
+        <Heading mb="4">March 2021</Heading>
         <Achievement text="Contributions and preparation for GSoC" />
         <Achievement text="Contributions to Outline" />
         <Achievement text="Boring college chores" />
-        <Heading mb="4">February 2020</Heading>
+        <Heading mb="4">February 2021</Heading>
         <Achievement text="Start building Btech final year project" />
         <Achievement text="Contributions to Outline" />
         <Achievement text="Read Alex Rauschmayer's JS for impatient programmer" />
-        <Heading mb="4">January 2020</Heading>
+        <Heading mb="4">January 2021</Heading>
         <Achievement text="Nothing much, wasted a lot of time." />
         <Heading mb="4">December 2020</Heading>
         <Achievement text="Busy with university exams" />
