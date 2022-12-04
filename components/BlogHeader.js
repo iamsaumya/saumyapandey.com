903a925d35a452fc56b03569f4c27749977039d3
Saumya Pandey <saumya.pandey@lyearn.com>
2022-12-04 17:42:32 +0530

    fix: add nextjs build blog

--- components/BlogHeader.js
+++ components/BlogHeader.js
@@ -2,29 +2,20 @@ import React from "react";
 import styled, { css } from "styled-components";
 import { breakpoint } from "styled-components-breakpoint";
 import Flex from "../components/Flex";
+import { getDescriptiveDate } from "../utils";
 
 const BlogHeader = ({ frontMatter }) => {
   return (
     <Container column>
       <Heading>{frontMatter.title}</Heading>
       <BlogMeta row gap={0.5}>
-        <Flex align="center">
-          <Author align="center">
-            <Image alt="Saumya Pandey" src="/static/images/profilepic.jpeg" />
-            Saumya Pandey
-          </Author>
-        </Flex>
-        <Text>{frontMatter.modifiedAt}</Text>
+        <Text>{getDescriptiveDate(frontMatter.modifiedAt)}</Text>
       </BlogMeta>
     </Container>
   );
 };
 
-const Badge = css`
-  padding: 4px 8px;
-  border: 1px solid #484a51;
-  border-radius: 4px;
-`;
+const Badge = css``;
 
 const Text = styled.p`
   font-weight: lighter;
@@ -32,26 +23,15 @@ const Text = styled.p`
   ${Badge}
 `;
 
-const Author = styled(Flex)`
-  font-weight: lighter;
-  ${Badge}
-`;
-
 const BlogMeta = styled(Flex)`
   width: 100%;
+  margin-bottom: 1rem;
 `;
 
 const Container = styled(Flex)`
   max-width: 700px;
   padding: 0 1rem;
   width: 100%;
-`;
-
-const Image = styled.img`
-  border-radius: 9999px;
-  width: 21px;
-  height: 21px;
-  margin-right: 0.5rem;
 `;
 
 const Heading = styled.h1`
