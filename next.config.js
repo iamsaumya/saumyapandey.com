7614ab9ac9fba2256f266b4cd0884be2d786a649
Saumya Pandey <pandeyysaumya@gmail.com>
2023-02-16 02:35:38 +0530

    fix: use preact

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+const withPreact = require("next-plugin-preact");
+module.exports = withPreact({
+  swcMinify: true,
   webpack: (config, { isServer }) => {
     if (isServer) {
       require("./scripts/generate-sitemap");
     }
+
     return config;
   },
-};
+});
